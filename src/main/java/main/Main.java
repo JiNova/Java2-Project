f63f606aadf24008f07459a0f4087538168efc17
EvoF <e.filippidis@web.de>
2017-07-17 11:11:44 +0200

    Flaticon Image

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,6 +20,7 @@ public class Main extends Application {
             Scene scene1 = new Scene(root);
             scene1.setRoot(root);
             primaryStage.setScene(scene1);
+            primaryStage.getIcons().add(new Image("file:icon.png"));
             primaryStage.sizeToScene();
             primaryStage.setTitle("Search and Enjoy");
             primaryStage.show();
