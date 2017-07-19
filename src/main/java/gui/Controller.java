259f051e3bdad06e20b896229fcdf5c87c152732
Andy <andy.brandner223@gmail.com>
2017-07-19 16:00:43 +0200

    - Added jsoup to maven

--- src/main/java/gui/Controller.java
+++ src/main/java/gui/Controller.java
@@ -7,7 +7,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
