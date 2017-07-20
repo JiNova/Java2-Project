b5bb1c1bec4594d38a5158fbf9de5092f8bf3d0c
Andy <andy.brandner223@gmail.com>
2017-07-20 16:51:12 +0200

    - Implemented lemma-search

--- src/main/java/searcher/Searcher.java
+++ src/main/java/searcher/Searcher.java
@@ -53,27 +53,8 @@ public class Searcher {
 
                         for (int i = 0; i < wordsOnLine.length; i++) {
                             if (wordsOnLine[i].equalsIgnoreCase(targetWord)) {
-                                SearchResult result = new SearchResult(i, wordsOnLine[i], sentences[sentenceId], wordsOnLine);
 
-                                String precWord = "";
-                                String folWord = "";
-
-                                if (i != 0) { //wenn wort nicht am satzanfang ist
-                                    precWord = wordsOnLine[i - 1];
-                                }
-
-                                if (i + 1 != wordsOnLine.length) { //wenn i+1 nicht das ende von dem array ist
-                                    folWord = wordsOnLine[i + 1];
-                                }
-
-                                String words[] = {precWord, targetWord, folWord};
-                                Map<String, String> wordsAndTags = Parser.getWordsTag(words);
-
-                                result.setTargetTag(wordsAndTags.get(targetWord));
-                                result.setPrecTag(wordsAndTags.get(precWord));
-                                result.setFolTag(wordsAndTags.get(folWord));
-
-                                foundTargets.add(result);
+                                foundTargets.add(saveResult(i, wordsOnLine[i], sentences[sentenceId], wordsOnLine));
                             }
                         }
                     }
@@ -106,94 +87,89 @@ public class Searcher {
         }
     }
 
-//    public static ArrayList<SearchResult> searchForTargetLemma(final String targetLemma, final String path, final TextProviderFactory.PROVIDER_TYPES providerType)
-//    {
-//        System.out.println("Searching for lemma: " + targetLemma);
-//
-//        BufferedReader br = null;
-//
-//        try {
-//
-//            TextProvider provider = TextProviderFactory.getTextProvider(path, providerType);
-//
-//            br = new BufferedReader(provider.getContentReader());
-//
-//            String[] wordsOnLine;
-//            String line;
-//            ArrayList<SearchResult> foundTargets = new ArrayList<SearchResult>();
-//
-//            while ((line = br.readLine()) != null) {
-//
-//                String[] sentences = null;
-//
-//                if (StringUtils.containsIgnoreCase(line, targetLemma))
-//                {
-//                    sentences = Parser.sentenceSplitter(line);
-//                }
-//                else
-//                {
-//                    continue;
-//                }
-//
-//                for (int sentenceId = 0; sentenceId < sentences.length; sentenceId++) {
-//
-//                    if (StringUtils.containsIgnoreCase(sentences[sentenceId], targetWord)) {
-//                        wordsOnLine = Parser.tokenizer(sentences[sentenceId]); //split the line into separate words
-//
-//                        for (int i = 0; i < wordsOnLine.length; i++) {
-//                            if (wordsOnLine[i].equalsIgnoreCase(targetWord)) {
-//                                SearchResult result = new SearchResult(i, wordsOnLine[i], sentences[sentenceId], wordsOnLine);
-//
-//                                String precWord = "";
-//                                String folWord = "";
-//
-//                                if (i != 0) { //wenn wort nicht am satzanfang ist
-//                                    precWord = wordsOnLine[i - 1];
-//                                }
-//
-//                                if (i + 1 != wordsOnLine.length) { //wenn i+1 nicht das ende von dem array ist
-//                                    folWord = wordsOnLine[i + 1];
-//                                }
-//
-//                                String words[] = {precWord, targetWord, folWord};
-//                                Map<String, String> wordsAndTags = Parser.getWordsTag(words);
-//
-//                                result.setTargetTag(wordsAndTags.get(targetWord));
-//                                result.setPrecTag(wordsAndTags.get(precWord));
-//                                result.setFolTag(wordsAndTags.get(folWord));
-//
-//                                System.out.println(Parser.getLemma(new String[]{targetWord}, new String[]{wordsAndTags.get(targetWord)})[0]);
-//
-//                                foundTargets.add(result);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
-//            return foundTargets;
-//        }
-//        catch (FileNotFoundException e)
-//        {
-//            throw new FileNotFoundException("Could not find file");
-//        }
-//        catch (IOException e)
-//        {
-//            throw new IOException("Error while accessing file");
-//        }
-//        finally
-//        {
-//            if (br != null)
-//            {
-//                try
-//                {
-//                    br.close();
-//                }
-//                catch (IOException e)
-//                {
-//                    throw new IOException("Could not close BufferedReader");
-//                }
-//            }
-//        }
-//    }
+    public static ArrayList<SearchResult> searchForTargetLemma(final String targetLemma, final String path, final TextProviderFactory.PROVIDER_TYPES providerType) throws IOException {
+        System.out.println("Searching for lemma: " + targetLemma);
+
+        BufferedReader br = null;
+
+        try {
+
+            TextProvider provider = TextProviderFactory.getTextProvider(path, providerType);
+
+            br = new BufferedReader(provider.getContentReader());
+
+            String[] wordsOnLine;
+            String line;
+            ArrayList<SearchResult> foundTargets = new ArrayList<SearchResult>();
+
+            while ((line = br.readLine()) != null) {
+
+                String[] sentences = Parser.sentenceSplitter(line);
+
+                for (int sentenceId = 0; sentenceId < sentences.length; sentenceId++) {
+
+                    wordsOnLine = Parser.tokenizer(sentences[sentenceId]); //split the line into separate words
+                    String[] tagResult = Parser.getPosTag(wordsOnLine);
+                    String[] lemmaResult = Parser.getLemma(wordsOnLine, tagResult);
+
+                        for (int i = 0; i < lemmaResult.length; i++) {
+
+                            if (targetLemma.equalsIgnoreCase(lemmaResult[i]))
+                            {
+                                foundTargets.add(saveResult(i, wordsOnLine[i], sentences[sentenceId], wordsOnLine));
+                            }
+                        }
+                    }
+                }
+
+            return foundTargets;
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new FileNotFoundException("Could not find file");
+        }
+        catch (IOException e)
+        {
+            throw new IOException("Error while accessing file");
+        }
+        finally
+        {
+            if (br != null)
+            {
+                try
+                {
+                    br.close();
+                }
+                catch (IOException e)
+                {
+                    throw new IOException("Could not close BufferedReader");
+                }
+            }
+        }
+    }
+
+    private static SearchResult saveResult(final int wordIndex, final String word, final String sentence, final String[] sentenceParts)
+    {
+        SearchResult result = new SearchResult(wordIndex, word, sentence, sentenceParts);
+
+        String precWord = "";
+        String folWord = "";
+
+        if (wordIndex != 0) { //wenn wort nicht am satzanfang ist
+            precWord = sentenceParts[wordIndex - 1];
+        }
+
+        if (wordIndex + 1 != sentenceParts.length) { //wenn i+1 nicht das ende von dem array ist
+            folWord = sentenceParts[wordIndex + 1];
+        }
+
+        String words[] = {precWord, word, folWord};
+        Map<String, String> wordsAndTags = Parser.getWordsTag(words);
+
+        result.setTargetTag(wordsAndTags.get(word));
+        result.setPrecTag(wordsAndTags.get(precWord));
+        result.setFolTag(wordsAndTags.get(folWord));
+
+        return result;
+    }
 }
